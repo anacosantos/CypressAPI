d65b3934a6b522c8e6a59d3e731b075451fdc035
anacosantos <anacarolinaosantos85@gmail.com>
2021-05-25 16:12:04 +0300

    Mocking API response cont.

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -11,7 +11,7 @@ describe('Test with backend', () => {
         cy.loginToApplication()
     })
 
-    it('verufy correct request and response', () => {
+    it('verify correct request and response', () => {
         
         //create cypress server
         cy.server()
@@ -31,8 +31,6 @@ describe('Test with backend', () => {
             expect(xhr.request.body.article.body).to.equal('This is a body of the Article')
             expect(xhr.response.body.article.description).to.equal('This is a description')
         })
-
-        
     })
 
     it('should gave tag with routing object', () =>{  
@@ -41,5 +39,24 @@ describe('Test with backend', () => {
         .should('contain', 'cypress')
         .and('contain', 'automation')
         .and('contain', 'testing')
+    })
+
+    it('verify global feed likes count', () => {
+        //create route for our articles laced
+        //get response information from feed(xhr)
+        //cy.screenshot('before-fill-in-article')
+        cy.route('GET', '**/articles/feed*', '{"articles":[],"articlesCount":0}')
+        //get article.json from global feed response(xhr)
+        cy.route('GET', '**/articles*', 'fixture:articles.json')
+
+        cy.contains('Global Feed').click()
+        //cy.screenshot('after-fill-in-article')
+
+        //get number of likes
+        cy.get('app-article-list button').then(listOfButtons => {
+            expect(listOfButtons[0]).to.contain('5')
+            expect(listOfButtons[1]).to.contain('3')
+        })
+
     })
 })
