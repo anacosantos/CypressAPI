03a33f3e628c60202c0dc5af956ac213a1b6c3a1
anacosantos <anacarolinaosantos85@gmail.com>
2021-05-25 07:46:33 +0300

    First API test

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,14 +1,31 @@
 /// <reference types="cypress"/>
 
+
 describe('Test with backend', () => {
 
     beforeEach('Login to the app', () => {
         cy.loginToApplication()
     })
 
-    it('should log in', () => {
-        cy.log('Yeeey we logged in!')
+    it('verufy correct request and response', () => {
+        
+        //create cypress server
+        cy.server()
+        //Request post URL: https://conduit.productionready.io/api/articles/
+        cy.route('POST', '**/articles').as('postArticles')
+
+        cy.contains('New Article').click()
+        cy.get('[formcontrolname="title"]').type('This is a title')
+        cy.get('[formcontrolname="description"]').type('This is a description')
+        cy.get('[formcontrolname="body"]').type('This is a body of the Article')
+        cy.contains('Publish Article').click()
+        //after click wait until this call will be completed, called as(alias) on cy.wait
+        cy.wait('@postArticles')
+        cy.get('@postArticles').then( xhr => {
+            console.log(xhr)
+            expect(xhr.status).to.equal(200)
+            expect(xhr.request.body.article.body).to.equal('This is a body of the Article')
+            expect(xhr.response.body.article.description).to.equal('This is a description')
+        })
     })
 })
-
-// go to network and filter ALL
