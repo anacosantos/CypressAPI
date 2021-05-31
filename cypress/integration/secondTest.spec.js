a1fb28c540c0f66be4729ad442d7fb9ed04075dc
anacosantos <anacarolinaosantos85@gmail.com>
2021-05-31 13:49:47 +0300

    CLI cypress

---
+++ cypress/integration/secondTest.spec.js
@@ -0,0 +1,15 @@
+
+
+describe('Test log out', () => {
+    
+    beforeEach('login to the app', ()=> {
+        cy.loginToApplication()
+    })
+    
+    it('verify use can log out sucessfully', ()=> {
+        cy.contains('Settings').click()
+        cy.contains('Or click here to logout').click()
+        cy.get('.navbar-nav').should('contain', 'Sign up')
+    }) 
+   
+})
